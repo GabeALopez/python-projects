7f5d35a95f5e80747d95eca2addc73535a86c03e
Gabriel <gabriel.andres.lopez03@gmail.com>
2023-03-22 21:17:19 -0400

    Finished first problem

---
+++ puzzles/exercise1.py
@@ -0,0 +1,19 @@
+list = [19, 19, 15, 5, 3, 5, 5, 2]
+list2 = [19, 15, 15, 5, 3, 3, 5, 2]
+list3 = [19, 19, 5, 5, 5, 5, 5]
+count1 = 0
+count2 = 0
+tag1 = 0
+tag2 = 0
+
+for x in list3:
+    if(x == 19):
+        count1 +=1 
+    elif(x == 5):
+        count2 += 1
+
+
+if(count1 == 2 and count2 == 3):
+    print("True")
+else:
+    print("False")
